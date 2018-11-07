a2043daba1c886007ef8ed42c94f7ec123a23ac4
Kris <Krisscholte@hotmail.com>
2018-11-07 16:13:53 +0100

    Added shell program

--- leds.sh
+++ leds.sh
@@ -4,6 +4,6 @@
 while true; do
   echo What would you like to do with leds? Type "1 1 1" to turn them all on, "0 0 0" to turn them off etc.
   read leds
-  echo leds > /sys/gpiodriver/leds
+  echo $leds > /sys/gpiodriver/leds
 
 done

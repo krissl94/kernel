63064b578d50918c497c537eb0b6f0731881c91a
krissl94 <krisscholte@hotmail.com>
2018-11-07 13:39:31 +0100

    Got rid of pix

--- driver_module.c
+++ driver_module.c
@@ -1,20 +1,20 @@
 #include </home/pi/kerneldriver/final/sysfs.c>
 
-static int pix_init(void){
-  printk(KERN_INFO "PIX: staring...");
+static int gpiodriver_init(void){
+  printk(KERN_INFO "GPIODRIVER: staring...");
   gpio_init();
   gpiodriver_sysfs_init();
-  printk(KERN_INFO "PIX: staring done.");
+  printk(KERN_INFO "GPIODRIVER: staring done.");
   return 0;
 }
 
-static void pix_exit(void){
-  printk(KERN_INFO "PIX: stopping...");
+static void gpiodriver_exit(void){
+  printk(KERN_INFO "GPIODRIVER: stopping...");
   gpio_cleanup();
   gpiodriver_sysfs_exit();
-  printk(KERN_INFO "PIX: stopping done.");
+  printk(KERN_INFO "GPIODRIVER: stopping done.");
 }
 
 
-module_init(pix_init);
-module_exit(pix_exit);
+module_init(gpiodriver_init);
+module_exit(gpiodriver_exit);
